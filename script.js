37e7c39e3c0fce12e03799568b8fdeba63a7af78
Melanie Gunn <melanie@melaniegunn.com>
2021-06-18 09:12:33 -0500

    added Shuttle Status info.  Code still not visible.

--- script.js
+++ script.js
@@ -1,62 +1,66 @@
 
 window.addEventListener("load", function () {
    let form = document.querySelector('form');
-   const pilotNameInput = document.querySelector("input[name='pilotName']");
-   const copilotNameInput = document.querySelector("input[name='copilotName']");
-   const fuelLevelInput = document.querySelector("input[name='fuelLevel']");
-   const cargoMassInput = document.querySelector("input[name='cargoMass']");
+   const pilotNameInput = document.querySelector("input[name=pilotName]");
+   const copilotNameInput = document.querySelector("input[name=copilotName]");
+   const fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+   const cargoMassInput = document.querySelector("input[name=cargoMass]");
    const inputs = document.querySelectorAll("input");
-   const faultyItems = document.getElementsByClassName("faultyItems");
-   console.log(faultyItems)
+   const faultyItems = document.getElementById("faultyItems");
    let pilotStatus = document.getElementById("pilotStatus");
    let copilotStatus = document.getElementById("copilotStatus");
+   let launchStatus = this.document.getElementById("launchStatus");
 
    form.addEventListener("submit", function (event) {
-      let messages = []
+      faultyItems.style.visibility.value = "visible"
 
       if (!isNaN(pilotNameInput.value)) {
-         messages.push("Please try again.  Pilot Name field must include text.");
-         alert(messages);
-         event.preventDefault()
+         alert("Please try again.  Pilot Name field must include text.");
       } 
       if (!isNaN(copilotNameInput.value)) {
-         messages.push("Please try again.  CoPilot Name field must include text.");
-         alert(messages);
-         event.preventDefault()
+         alert("Please try again.  CoPilot Name field must include text.");  
       } 
-
-      //(!isNaN(copilotNameInput.value))
-
       if (isNaN(fuelLevelInput.value)) {
-         messages.push("Please try again.  Field must be a number.")
-         event.preventDefault()
+         alert("Please try again.  Field must be a number.");
       }
       if (isNaN(cargoMassInput.value)) {
-         messages.push("Please try again.  Field must be a number.")
-         event.preventDefault()
+         alert("Please try again.  Field must be a number."); 
       }
 
       inputs.forEach(input => {
          if (input.value == ""){
-            alert("All fields required!")
-            event.preventDefault()
+            alert("All fields required!");  
          }
-      })
+      })//end of form validation
 
-         function updateShuttleRequirements(){
-            
-         }
+      pilotStatus.innerHTML = `Pilot ${pilotNameInput.value} is ready for Launch.`
+      copilotStatus.innerHTML = `Co-pilot ${copilotNameInput.value} is ready for launch`
+
+      if (fuelLevelInput < 10000){
+         fuelStatus.innerHTML = "There is not enough fuel for the journey."
+         launchStatus.innerHTML = "Shuttle not Ready for Launch"
+         launchStatus.style.color = "red"
          faultyItems.style.visibility = "visible"
-         console.log(faultyItems.style.visibility);
-         pilotStatus.innerHTML = `Pilot ${pilotNameInput.value} is ready for Launch.`
-         copilotStatus.innerHTML = `Co-pilot ${copilotNameInput.value} is ready for launch`
-         console.log(pilotStatus.innerHTML,copilotStatus.innerHTML);
-         event.preventDefault()
+      }
       
-         
+      if (cargoMass > 10000) {
+         cargoStatus.innerHTML - "There is too much mass for the shuttle to take off"
+         launchStatus.innerHTML = "Shuttle not Ready for Launch"
+         launchStatus.style.color = "red"
+         faultyItems.style.visibility = "visible"
+      }
+
+      if (fuelLevelInput >= 10000 && cargoMass <= 10000){
+         launchStatus.style.color = "green"
+         launchStatus.innerHTML = "Shuttle is Ready for Launch"
+      }
+            
    });// end of submit add event listener
 });
 
+//notes
+//do visibility after data is set
+//if you have your code where oyu have a bunch of validators you need a way to track if it passed everything before you have the rest of the code moving forward
 
 //The list of shuttle requirements (a list means that it is an array), the div with the id faultyItems, should be updated if something is not ready for launch.
 
